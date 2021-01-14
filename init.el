03c5fb45275eab4933337b6882c8f38b2017e31e
Luca Cambiaghi <luca.cambiaghi@maersk.com>
2021-01-14 22:55:37 +0100

    evil-org-mode

--- init.el
+++ init.el
@@ -209,13 +209,15 @@
     "hF" 'describe-face
     "hk" 'describe-key
 
+    "o" '(:ignore t :which-key "org")
+
     "p" '(:ignore t :which-key "project")
 
     "s" '(:ignore t :which-key "search")
 
     "t"  '(:ignore t :which-key "toggle")
     "t d"  '(toggle-debug-on-error :which-key "debug on error")
-    "t v" '((lambda () (interactive) (visual-line-mode)) :wk "visual line")
+    "t v" '((lambda () (interactive) (visual-line-mode 'toggle)) :wk "visual line")
 
     "w" '(:ignore t :which-key "window")
     "wl"  'windmove-right
@@ -405,7 +407,8 @@
 
 (setq display-buffer-alist
       `((,(rx bos (or "*Apropos*" "*Help*" "*helpful" "*info*" "*Summary*") (0+ not-newline))
-         (display-buffer-reuse-mode-window display-buffer-pop-up-window)
+         (display-buffer-reuse-mode-window display-buffer-below-selected)
+         (window-height . 0.33)
          (mode apropos-mode help-mode helpful-mode Info-mode Man-mode))))
 
 (use-package winum
@@ -427,6 +430,15 @@
 :demand
 :config
 (persistent-scratch-setup-default))
+
+(use-package olivetti
+  :general
+  (my/leader-keys
+    "t o" '(olivetti-mode :wk "olivetti"))
+  :init
+  (setq olivetti-body-width 0.7)
+  (setq olivetti-minimum-body-width 80)
+  (setq olivetti-recall-visual-line-mode-entry-state t))
 
 (use-package selectrum
   :demand
@@ -461,7 +473,9 @@
 (use-package embark
   :after selectrum
   :demand
-  :general (selectrum-minibuffer-map "C-o" #'embark-act)
+  :general
+  (general-nmap "C-e" #'embark-act)
+  ;; (selectrum-minibuffer-map "C-o" #'embark-act)
   :config
   ;; For Selectrum users:
   (defun current-candidate+category ()
@@ -756,20 +770,22 @@ Current pattern: %`evil-mc-pattern
   :hook (dired-mode . all-the-icons-dired-mode))
 
 (use-package org
-  ;; :straight (:type built-in)
   :hook ((org-mode . my/org-mode-setup)
          (org-mode . prettify-symbols-mode)
          (org-mode . (lambda () (add-hook 'after-save-hook #'my/org-babel-tangle-config))))
   :general
   (my/leader-keys
-    "C" '(org-capture :wk "capture"))
+    "o c" '(org-capture :wk "capture")
+    "o a" '(org-agenda-list :wk "agenda"))
   (my/local-leader-keys
     :keymaps 'org-mode-map
-    "n" '(org-toggle-narrow-to-subtree :wk "narrow subtree"))
+    "n" '(org-toggle-narrow-to-subtree :wk "narrow subtree")
+    "t t" '(org-todo :wk "heading todo"))
   (org-mode-map
    :states '(normal)
    "z i" '(org-toggle-inline-images :wk "inline images"))
   :init
+  ;; general settings
   (setq org-directory "~/Dropbox/org"
         org-image-actual-width nil
         +org-export-directory "~/Dropbox/org/export"
@@ -778,7 +794,6 @@ Current pattern: %`evil-mc-pattern
         org-agenda-files '("~/dropbox/org/personal/tasks/birthdays.org" "~/dropbox/org/personal/tasks/todo.org" "~/dropbox/Notes/Test.inbox.org")
         ;; org-export-in-background t
         org-catch-invisible-edits 'smart)
-
   ;; disable modules for faster startup
   (setq org-modules
         '(;; ol-w3m
@@ -838,13 +853,11 @@ Current pattern: %`evil-mc-pattern
                     "%i%?"))
 
           ))
-
   (setq-default prettify-symbols-alist '(("#+BEGIN_SRC" . "»")
                                          ("#+END_SRC" . "«")
                                          ("#+begin_src" . "»")
                                          ("#+end_src" . "«")))
   (setq prettify-symbols-unprettify-at-point 'right-edge)
-
   ;; (setq org-agenda-custom-commands
   ;;         '(("d" "Dashboard"
   ;;            ((agenda "" ((org-deadline-warning-days 7)))
@@ -860,15 +873,18 @@ Current pattern: %`evil-mc-pattern
     (org-indent-mode)
     (variable-pitch-mode 1)
     (visual-line-mode 1))
-
   (defun my/org-babel-tangle-config ()
     (when (string-equal (file-name-directory (buffer-file-name))
                         (expand-file-name user-emacs-directory))
       ;; Dynamic scoping to the rescue
       (let ((org-confirm-babel-evaluate nil))
         (org-babel-tangle))))
-
   :config
+  ;; visual
+  (org-indent-mode)
+  (variable-pitch-mode 1)
+  (visual-line-mode 1)
+  ;; org habit
   (require 'org-habit)
   (add-to-list 'org-modules 'org-habit)
   ;; (efs/org-font-setup)
@@ -1008,6 +1024,7 @@ Current pattern: %`evil-mc-pattern
     (hide-mode-line-mode 1)
     (diff-hl-mode 0)
     (centaur-tabs-mode 0))
+  ;; TODO maybe also enable olivetti mode?
   (defun +remap-faces-at-start-present-term ()
     (interactive)
     (setq-local face-remapping-alist '((default (:height 1.50) variable-pitch)
@@ -1033,14 +1050,221 @@ Current pattern: %`evil-mc-pattern
   ;; (org-tree-slide-presentation-profile)
   )
 
-(use-package olivetti
+(use-package evil-org-mode
+  :straight (evil-org-mode :type git :host github :repo "hlissner/evil-org-mode")
+  :hook (org-mode . evil-org-mode)
   :general
-  (my/leader-keys
-    "t o" '(olivetti-mode :wk "olivetti"))
+  (general-nmap
+    :keymaps '(evil-org-mode-map org-mode-map )
+    "<C-return>"      #'+org/insert-item-below
+    "<C-S-return>"    #'+org/insert-item-above
+    "RET"   #'+org/dwim-at-point)
   :init
-  (setq olivetti-body-width 0.7)
-  (setq olivetti-minimum-body-width 80)
-  (setq olivetti-recall-visual-line-mode-entry-state t))
+  (defun +org--insert-item (direction)
+    (let ((context (org-element-lineage
+                    (org-element-context)
+                    '(table table-row headline inlinetask item plain-list)
+                    t)))
+      (pcase (org-element-type context)
+        ;; Add a new list item (carrying over checkboxes if necessary)
+        ((or `item `plain-list)
+         ;; Position determines where org-insert-todo-heading and org-insert-item
+         ;; insert the new list item.
+         (if (eq direction 'above)
+             (org-beginning-of-item)
+           (org-end-of-item)
+           (backward-char))
+         (org-insert-item (org-element-property :checkbox context))
+         ;; Handle edge case where current item is empty and bottom of list is
+         ;; flush against a new heading.
+         (when (and (eq direction 'below)
+                    (eq (org-element-property :contents-begin context)
+                        (org-element-property :contents-end context)))
+           (org-end-of-item)
+           (org-end-of-line)))
+
+        ;; Add a new table row
+        ((or `table `table-row)
+         (pcase direction
+           ('below (save-excursion (org-table-insert-row t))
+                   (org-table-next-row))
+           ('above (save-excursion (org-shiftmetadown))
+                   (+org/table-previous-row))))
+
+        ;; Otherwise, add a new heading, carrying over any todo state, if
+        ;; necessary.
+        (_
+         (let ((level (or (org-current-level) 1)))
+           ;; I intentionally avoid `org-insert-heading' and the like because they
+           ;; impose unpredictable whitespace rules depending on the cursor
+           ;; position. It's simpler to express this command's responsibility at a
+           ;; lower level than work around all the quirks in org's API.
+           (pcase direction
+             (`below
+              (let (org-insert-heading-respect-content)
+                (goto-char (line-end-position))
+                (org-end-of-subtree)
+                (insert "\n" (make-string level ?*) " ")))
+             (`above
+              (org-back-to-heading)
+              (insert (make-string level ?*) " ")
+              (save-excursion (insert "\n"))))
+           (when-let* ((todo-keyword (org-element-property :todo-keyword context))
+                       (todo-type    (org-element-property :todo-type context)))
+             (org-todo
+              (cond ((eq todo-type 'done)
+                     ;; Doesn't make sense to create more "DONE" headings
+                     (car (+org-get-todo-keywords-for todo-keyword)))
+                    (todo-keyword)
+                    ('todo)))))))
+
+      (when (org-invisible-p)
+        (org-show-hidden-entry))
+      (when (and (bound-and-true-p evil-local-mode)
+                 (not (evil-emacs-state-p)))
+        (evil-insert 1))))
+
+  (defun +org/insert-item-below (count)
+    "Inserts a new heading, table cell or item below the current one."
+    (interactive "p")
+    (dotimes (_ count) (+org--insert-item 'below)))
+
+  (defun +org/insert-item-above (count)
+    "Inserts a new heading, table cell or item above the current one."
+    (interactive "p")
+    (dotimes (_ count) (+org--insert-item 'above)))
+
+  (defun +org/dwim-at-point (&optional arg)
+    "Do-what-I-mean at point.
+If on a:
+- checkbox list item or todo heading: toggle it.
+- clock: update its time.
+- headline: cycle ARCHIVE subtrees, toggle latex fragments and inline images in
+  subtree; update statistics cookies/checkboxes and ToCs.
+- footnote reference: jump to the footnote's definition
+- footnote definition: jump to the first reference of this footnote
+- table-row or a TBLFM: recalculate the table's formulas
+- table-cell: clear it and go into insert mode. If this is a formula cell,
+  recaluclate it instead.
+- babel-call: execute the source block
+- statistics-cookie: update it.
+- latex fragment: toggle it.
+- link: follow it
+- otherwise, refresh all inline images in current tree."
+    (interactive "P")
+    (let* ((context (org-element-context))
+           (type (org-element-type context)))
+      ;; skip over unimportant contexts
+      (while (and context (memq type '(verbatim code bold italic underline strike-through subscript superscript)))
+        (setq context (org-element-property :parent context)
+              type (org-element-type context)))
+      (pcase type
+        (`headline
+         (cond ((memq (bound-and-true-p org-goto-map)
+                      (current-active-maps))
+                (org-goto-ret))
+               ((and (fboundp 'toc-org-insert-toc)
+                     (member "TOC" (org-get-tags)))
+                (toc-org-insert-toc)
+                (message "Updating table of contents"))
+               ((string= "ARCHIVE" (car-safe (org-get-tags)))
+                (org-force-cycle-archived))
+               ((or (org-element-property :todo-type context)
+                    (org-element-property :scheduled context))
+                (org-todo
+                 (if (eq (org-element-property :todo-type context) 'done)
+                     (or (car (+org-get-todo-keywords-for (org-element-property :todo-keyword context)))
+                         'todo)
+                   'done))))
+         ;; Update any metadata or inline previews in this subtree
+         (org-update-checkbox-count)
+         (org-update-parent-todo-statistics)
+         (when (and (fboundp 'toc-org-insert-toc)
+                    (member "TOC" (org-get-tags)))
+           (toc-org-insert-toc)
+           (message "Updating table of contents"))
+         (let* ((beg (if (org-before-first-heading-p)
+                         (line-beginning-position)
+                       (save-excursion (org-back-to-heading) (point))))
+                (end (if (org-before-first-heading-p)
+                         (line-end-position)
+                       (save-excursion (org-end-of-subtree) (point))))
+                (overlays (ignore-errors (overlays-in beg end)))
+                (latex-overlays
+                 (cl-find-if (lambda (o) (eq (overlay-get o 'org-overlay-type) 'org-latex-overlay))
+                             overlays))
+                (image-overlays
+                 (cl-find-if (lambda (o) (overlay-get o 'org-image-overlay))
+                             overlays)))
+           (+org--toggle-inline-images-in-subtree beg end)
+           (if (or image-overlays latex-overlays)
+               (org-clear-latex-preview beg end)
+             (org--latex-preview-region beg end))))
+
+        (`clock (org-clock-update-time-maybe))
+
+        (`footnote-reference
+         (org-footnote-goto-definition (org-element-property :label context)))
+
+        (`footnote-definition
+         (org-footnote-goto-previous-reference (org-element-property :label context)))
+
+        ((or `planning `timestamp)
+         (org-follow-timestamp-link))
+
+        ((or `table `table-row)
+         (if (org-at-TBLFM-p)
+             (org-table-calc-current-TBLFM)
+           (ignore-errors
+             (save-excursion
+               (goto-char (org-element-property :contents-begin context))
+               (org-call-with-arg 'org-table-recalculate (or arg t))))))
+
+        (`table-cell
+         (org-table-blank-field)
+         (org-table-recalculate arg)
+         (when (and (string-empty-p (string-trim (org-table-get-field)))
+                    (bound-and-true-p evil-local-mode))
+           (evil-change-state 'insert)))
+
+        (`babel-call
+         (org-babel-lob-execute-maybe))
+
+        (`statistics-cookie
+         (save-excursion (org-update-statistics-cookies arg)))
+
+        ((or `src-block `inline-src-block)
+         (org-babel-execute-src-block arg))
+
+        ((or `latex-fragment `latex-environment)
+         (org-latex-preview arg))
+
+        (`link
+         (let* ((lineage (org-element-lineage context '(link) t))
+                (path (org-element-property :path lineage)))
+           (if (or (equal (org-element-property :type lineage) "img")
+                   (and path (image-type-from-file-name path)))
+               (+org--toggle-inline-images-in-subtree
+                (org-element-property :begin lineage)
+                (org-element-property :end lineage))
+             (org-open-at-point arg))))
+
+        ((guard (org-element-property :checkbox (org-element-lineage context '(item) t)))
+         (let ((match (and (org-at-item-checkbox-p) (match-string 1))))
+           (org-toggle-checkbox (if (equal match "[ ]") '(16)))))
+
+        (_
+         (if (or (org-in-regexp org-ts-regexp-both nil t)
+                 (org-in-regexp org-tsr-regexp-both nil  t)
+                 (org-in-regexp org-link-any-re nil t))
+             (call-interactively #'org-open-at-point)
+           (+org--toggle-inline-images-in-subtree
+            (org-element-property :begin context)
+            (org-element-property :end context)))))))
+
+  :config
+  (add-hook 'evil-org-mode-hook #'evil-normalize-keymaps)
+  )
 
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
@@ -1051,14 +1275,18 @@ Current pattern: %`evil-mc-pattern
     "c" '(:keymap lsp-command-map))
   (my/local-leader-keys
     :keymaps 'lsp-mode-map
-    "r" '(lsp-rename :wk "rename"))
+    "r" '(lsp-rename :wk "rename")
+    "i" '(:ignore t :which-key "import")
+    "i o" '(lsp-rename :wk "optimize"))
   (lsp-mode-map
-   "gd" 'lsp-lsp-find-definition
+   :states '(normal)
+   "gd" 'lsp-find-definition
    "gD" 'lsp-find-references)
   :init
   (setq lsp-restart 'ignore)
   (setq lsp-eldoc-enable-hover nil)
   (setq lsp-enable-file-watchers nil)
+  (setq lsp-signature-auto-activate nil)
   :config
   (lsp-enable-which-key-integration t))
 
@@ -1084,7 +1312,7 @@ Current pattern: %`evil-mc-pattern
     "d r" '(dap-ui-repl :wk "repl")
     "d h" '(dap-hydra :wk "hydra"))
   :init
-  (setq dap-auto-configure-features '(sessions locals breakpoints repl))
+  (setq dap-auto-configure-features '(locals repl))
   (setq dap-python-debugger 'debugpy)
   ;; show stdout
   (setq dap-auto-show-output t)
@@ -1235,6 +1463,12 @@ Current pattern: %`evil-mc-pattern
         (jupyter-repl-pop-to-buffer)
       (call-interactively 'jupyter-repl-associate-buffer)))
   (advice-add 'jupyter-command :override #'jupyter-command-venv))
+
+(use-package pyimport
+  :general
+  (my/local-leader-keys
+    :keymaps 'python-mode-map
+    "i i" '(pyimport-insert-missing :wk "autoimport")))
 
 (use-package ess
     :init
