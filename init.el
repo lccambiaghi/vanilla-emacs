e0eb7290fbd1a6d94e770fe0c77d44fccb8ad373
Luca Cambiaghi <luca.cambiaghi@maersk.com>
2021-03-09 14:00:23 +0100

    Checkpoint pre custom-id

--- init.el
+++ init.el
@@ -1116,7 +1116,8 @@ asynchronously."
   :hook (org-mode . org-html-themify-mode)
   :init
   (setq org-html-themify-themes
-        '(;; (dark . modus-vivendi)
+        '(;; (light . modus-operandi)
+					;; (dark . modus-vivendi)
           (light . modus-operandi)
           (dark . modus-operandi)
 					))
@@ -1204,6 +1205,51 @@ asynchronously."
     (remove-hook 'after-save-hook 'ag/reveal-and-move-back))
   (add-hook 'org-babel-pre-tangle-hook 'ag/org-reveal-after-save-on)
   (add-hook 'org-babel-post-tangle-hook 'ag/org-reveal-after-save-off)
+  )
+
+(use-package org
+  :init
+  (defun my/org-custom-id-get (&optional pom create prefix)
+    "Get the CUSTOM_ID property of the entry at point-or-marker POM.
+   If POM is nil, refer to the entry at point. If the entry does
+   not have an CUSTOM_ID, the function returns nil. However, when
+   CREATE is non nil, create a CUSTOM_ID if none is present
+   already. PREFIX will be passed through to `org-id-new'. In any
+   case, the CUSTOM_ID of the entry is returned."
+    (interactive)
+    (org-with-point-at pom
+      (let ((id (org-entry-get nil "CUSTOM_ID")))
+        (cond
+         ((and id (stringp id) (string-match "\\S-" id))
+          id)
+         (create
+          (setq id (org-id-new (concat prefix "h")))
+          (org-entry-put pom "CUSTOM_ID" id)
+          (org-id-add-location id (buffer-file-name (buffer-base-buffer)))
+          id)))))
+  
+  (defun my/org-add-ids-to-headlines-in-file ()
+    "Add CUSTOM_ID properties to all headlines in the current
+   file which do not already have one. Only adds ids if the
+   `auto-id' option is set to `t' in the file somewhere. ie,
+   #+OPTIONS: auto-id:t"
+    (interactive)
+    (save-excursion
+      (widen)
+      (goto-char (point-min))
+      (when (re-search-forward "^#\\+OPTIONS:.*auto-id:t" 10000 t)
+        (org-map-entries (lambda () (my/org-custom-id-get (point) 'create))))))
+  :config
+  (require 'org-id)
+  (setq org-id-link-to-org-use-id 'create-if-interactive-and-no-custom-id)
+
+  ;; (add-hook 'org-mode-hook
+  ;;           (lambda ()
+  ;;             (add-hook 'before-save-hook
+  ;;                       (lambda ()
+  ;;                         (when (and (eq major-mode 'org-mode)
+  ;;                                    (eq buffer-read-only nil))
+  ;;                           (my/org-add-ids-to-headlines-in-file))))))
   )
 
 (use-package all-the-icons)
